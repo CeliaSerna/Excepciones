9bd9b4330b935e9c3356faddf79e1b732830a61f
PROGRAMACION <PROGRAMACION@AT1-PC7>
2023-05-03 14:19:32 +0200

    copia

--- src/com/cursoceat/controller/ArchivoBuffer.java
+++ src/com/cursoceat/controller/ArchivoBuffer.java
@@ -11,11 +11,25 @@ BufferedReader in=null;
 String texto="";
 try {
 	in= new BufferedReader(new FileReader("C:\\Users\\PROGRAMACION\\Documents\\prueba.txt"));
+	String linea = in.readLine();
+	while(linea!=null) {
+		
+	 texto+=linea+"\n";
+	linea=in.readLine();
+	}
 	
 }catch(IOException e) {
 	System.out.println(e);
+}finally {
+	if(in!=null) {
+		try {
+			in.close();
+		}catch (IOException e) {
+			System.out.println(e);
+		}
+	}
 }
-
+System.out.println(texto);
 
 
 }
